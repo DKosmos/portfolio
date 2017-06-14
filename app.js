0f3720e2312f8a046f4806a53dd7724cc10355d5
DKosmos <davidakosmos@gmail.com>
2017-06-14 08:20:03 -0700

    adds scripts to html and pushes portfolioData into items
    adds jquery and portfolioItems scripts to index.html
    gives .template class a display of none
    pushes portfolio data into an items array

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 'use strict'
 
-function PortfolioItem(name, url, img, title, description){
-  this.name = name;
-  this.url = url;
-  this.img = img;
-  this.title = title;
-  this.description = description;
+var items = [];
+
+function PortfolioItem(portfolioDataObj){
+  this.name = portfolioDataObj.name;
+  this.url = portfolioDataObj.url;
+  this.img = portfolioDataObj.img;
+  this.title = portfolioDataObj.title;
+  this.description = portfolioDataObj.description;
 }
+
+portfolioData.forEach(function(itemObj) {
+  items.push(new PortfolioItem(itemObj));
+});
