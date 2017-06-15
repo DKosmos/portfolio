7e406b68e124d23209a924a2a94ba7d212223a60
DKosmos <davidakosmos@gmail.com>
2017-06-14 20:41:07 -0700

    adds navbar single-page view
    adds classes and data to tabs and sections to prime for jQuery
    shows or hides sections based on home, about, or portfolio class

---
+++ scripts/viewcontrol.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var portfolioView = {};
+
+portfolioView.handleMainNav = function() {
+  $('.navbar .tab').click(function(){
+    var content = $(this).attr('data-content');
+    $('.content').each(function(){
+      if($(this).hasClass(content)){
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+}
+
+$(document).ready(function(){
+  portfolioView.handleMainNav();
+  $('.content').each(function(){
+    $(this).hide();
+  });
+})
