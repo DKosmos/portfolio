49873d6bb49f927657d2bb708689132158cd91c9
DKosmos <davidakosmos@gmail.com>
2017-06-20 11:14:59 -0700

    adds JSON/AJAX functionality to data managment

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,5 @@
 'use strict'
 
-var items = [];
-
 function PortfolioItem(portfolioDataObj){
   this.name = portfolioDataObj.name;
   this.url = portfolioDataObj.url;
@@ -10,19 +8,32 @@ function PortfolioItem(portfolioDataObj){
   this.description = portfolioDataObj.description;
 }
 
+PortfolioItem.all = [];
+
 PortfolioItem.prototype.toHtml = function() {
   var template = $('#portfolio-template').html();
   var renderTemplate = Handlebars.compile(template);
   return renderTemplate(this);
 }
+PortfolioItem.loadAll = function(rawData){
+  rawData.forEach(function(itemObj) {
+    PortfolioItem.all.push(new PortfolioItem(itemObj));
+  });
+  PortfolioItem.all.forEach(function(item) {
+    $('.portfolio').append(item.toHtml());
+  });
+};
 
-portfolioData.forEach(function(itemObj) {
-  items.push(new PortfolioItem(itemObj));
-});
-
-items.forEach(function(item) {
-  $('.portfolio').append(item.toHtml());
-})
+PortfolioItem.fetchAll = function() {
+  if(localStorage.rawData){
+    PortfolioItem.loadAll(JSON.parse(localStorage.rawData));
+  } else {
+    $.getJSON('../scripts/portfolioitems.json').then(function(data){
+      localStorage.rawData = JSON.stringify(data);
+      PortfolioItem.loadAll(data);
+    })
+  }
+}
 
 $('div.icon-menu').on('click', function(){
   $('.navbar').toggleClass('hidden');
