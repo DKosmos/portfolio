445eab25ce4a1283e3903d2ad361af4357262a3f
DKosmos <davidakosmos@gmail.com>
2017-06-20 19:47:09 -0700

    adds node.js server hosting with express.js and body-parser.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser').urlencoded({extended: true});
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, function(){
+  console.log('You are on port:', PORT);
+});
+
+app.use(function(req, res, next){
+  res.status(404).send('404 File not found');
+});
